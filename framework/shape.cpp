7b1043d2ca888d762d09c58c01ec2dd15319028d
ANNI90123 <ajngit@mail.n-ithing.de>
2023-06-19 14:13:03 +0200

    Add print method to Shape class andadd operator<<

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -5,3 +5,11 @@ Shape::Shape(std::string const& name, glm::vec3 const& color):
 	color_(color){
 
 }
+
+std::ostream& Shape::print(std::ostream& os) const {
+	return os << "Shape: " << name_ << "\nFarbe: r(" << color_.r << ") g(" << color_.g << ") b(" << color_.b << ") \n\n";
+}
+
+std::ostream& operator<<(std::ostream& os, Shape const& s) {
+	return s.print(os);
+}
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -3,17 +3,23 @@
 
 #include <glm/vec3.hpp>
 #include <string>
+#include <iostream>
 
 class Shape {
 public:
 	Shape(std::string const& name, glm::vec3 const& color);
+
 	virtual float area() const = 0;
 	virtual float volume() const = 0;
+
+	virtual std::ostream& print(std::ostream& os) const;
 
 private:
 	std::string name_;
 	glm::vec3 color_;
 
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& s);
 
 #endif
